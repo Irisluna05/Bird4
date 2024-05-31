eae0840155eaa2e3481cd049ce832779198bdde5
Irisluna05 <145460302+Irisluna05@users.noreply.github.com>
2024-05-31 20:23:39 +0200

    Eindversie
    alles erin

---
+++ script.js
@@ -0,0 +1,71 @@
+const titel = document.querySelector("h1");
+let tamagochi = document.querySelector("#main");
+const btn = document.querySelector("#ketting");
+const btn2 = document.querySelector("#eten");
+const pElement = document.querySelector("#tekst");
+const restartBtn = document.querySelector("#restart");
+//--Bron Thomas Plas en Mozilla
+let muziekje = new Audio("audio/cardinal.mp3"); 
+let happy = new Audio("audio/happy.mp3")
+let hungerbalk = document.querySelector("#hunger");
+let hunger = 0;
+
+function verhoogHunger() {
+    if (hunger < 10) {
+        hunger += 1;
+    }
+    hungerbalk.value = hunger;
+    updateText();
+    checkHungerLevel();
+}
+
+setInterval(verhoogHunger, 2000); 
+
+function verlaagHunger() {
+    if (hunger > 0) {
+        hunger -= 1;
+        hungerbalk.value = hunger;
+        happy.play();
+        changeImage();
+        updateText();
+    }
+}
+
+function updateText() {
+    if (hunger > 5) {
+        pElement.textContent = "Ik heb honger";
+        tamagochi.src = './images/vogel2.png';
+    } else {
+        pElement.textContent = "Hallo";
+    }
+}
+
+function checkHungerLevel() {
+    if (hunger >= 10) {
+        tamagochi.src = './images/vogelsad.png';
+        pElement.textContent = "Ik wil nu ETEN";
+        restartBtn.style.visibility = 'visible'; 
+        btn2.disabled = true;
+    } else {
+                restartBtn.style.visibility = 'hidden';
+                btn2.disabled = false; 
+     }
+}
+
+function changeImage() {
+    tamagochi.src = './images/vogeleat.png';
+}
+
+function restartSpel() {
+    hunger = 0;
+    hungerbalk.value = hunger;
+    pElement.textContent = "Hallo";
+    tamagochi.src = './images/vogel.png';
+    restartBtn.style.visibility = 'hidden'; // Corrected typo here
+    btn2.disabled = false; 
+    muziekje.play();
+}
+
+btn2.addEventListener("click", verlaagHunger);
+restartBtn.addEventListener("click", restartSpel);
+
